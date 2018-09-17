762fd66feef650fba44510d5f92fe7570ee9d097
Dan Needham <dan.needham3@gmail.com>
2018-09-17 12:43:26 -0400

    all works

--- bubblesort.js
+++ bubblesort.js
@@ -1,14 +1,21 @@
 const bubbleSort = (arr) => {
     let counter = 0;
-    for (let i = 0; i<arr.length-counter; i++){
-        if (arr[i] > arr[i+1]){
-            swap(arr[i], arr[i+1])
+    let swaps = true
+    while(swaps){
+        swaps = false
+        for (let i = 0; i<(arr.length-counter); i++){
+            if (arr[i] > arr[i+1]){
+            arr = swap(arr,i,i+1)
+            swaps = true
+            }
         }
+        counter++
     }
+    return arr
 }
-
-const swap = (left, right) => {
-    const temp = left;
-    left = right;
-    right = temp;
+function swap(arr,leftIndex,rightIndex) {
+    const temp1 = arr[leftIndex];
+    arr[leftIndex] = arr[rightIndex];
+    arr[rightIndex] = temp1;
+    return arr
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,4 +1,7 @@
 describe('Bubble Sort', function(){
+    
+    
+    
     it('handles an empty array', function(){
       expect( bubbleSort([]) ).toEqual( [] );
     });
@@ -6,6 +9,13 @@ describe('Bubble Sort', function(){
 
     it('sorts an array with elements', function(){
       expect( bubbleSort([1,3,2,5,4])).toEqual( [1,2,3,4,5] );
-      }
-    )
-});
+      spyOn(window, 'swap').and.callThrough();
+      bubbleSort([1,3,2,5,4]);  
+      expect( swap.calls.count()).toEqual(2)
+    });
+
+    it('sorts a backwards array', function(){
+        expect( bubbleSort([5,4,3,2,1])).toEqual( [1,2,3,4,5])
+    });
+})
+
